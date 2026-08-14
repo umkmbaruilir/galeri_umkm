d718a57565638016449608ea2256c95ec7ece5b8
umkmbaruilir <umkmbaruilir@gmail.com>
2026-08-14 23:07:28 +0800

    troubleshoot

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
 };
 
 export const prisma =
-  globalForPrisma.prisma ||
+  globalForPrisma.prisma ??
   new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
